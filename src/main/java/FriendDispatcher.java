1547a248c9008ef8032de26dedd02e777b3903ea
David Morales <davidm62@usc.edu>
2022-05-06 15:29:01 -0700

    Create FriendDispatcher.java

---
+++ src/main/java/FriendDispatcher.java
@@ -0,0 +1,62 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.reflect.TypeToken;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Serial;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher; //added
+import javax.servlet.ServletConfig; //added
+import javax.servlet.ServletContext; //added
+import java.nio.charset.*; //added
+
+@WebServlet("/FriendDispatcher") //added
+public class FriendDispatcher extends HttpServlet {
+    @Serial
+    private static final long serialVersionUID = 1L;
+    
+    /**
+     * Default constructor.
+     */
+    public FriendDispatcher() {
+    	
+    }
+
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+     *      response)
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // TODO
+    	
+    	String email = request.getParameter("email");
+    	ArrayList<User> profiles = GetProfile.getFriends(email);
+    	
+    	request.setAttribute("friends", profiles);
+    	RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/friends.jsp");
+    	dispatcher.forward(request, response);
+    	
+    }
+   
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+     *      response)
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        doGet(request, response);
+    }
+}
