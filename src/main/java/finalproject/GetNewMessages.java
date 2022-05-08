9334cd2456072b44d102ac4c026dfa2ed249ed57
David Morales <davidm62@usc.edu>
2022-05-08 01:09:54 -0700

    Update GetNewMessages.java

--- src/main/java/finalproject/GetNewMessages.java
+++ src/main/java/finalproject/GetNewMessages.java
@@ -1,4 +1,3 @@
-package finalproject;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebInitParam;
@@ -7,8 +6,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
-import Util.Constant;
 
 import java.io.IOException;
 import java.io.PrintWriter;
