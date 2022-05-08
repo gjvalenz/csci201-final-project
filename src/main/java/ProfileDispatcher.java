f489009b509d3ac257b2e4e58fa2f47cbf39e376
David Morales <davidm62@usc.edu>
2022-05-08 01:18:19 -0700

    Update and rename src/main/java/finalproject/ProfileDispatcher.java to src/main/java/ProfileDispatcher.java

--- src/main/java/ProfileDispatcher.java
+++ src/main/java/ProfileDispatcher.java
@@ -1,4 +1,3 @@
-package finalproject;
 
 import javax.servlet.*;
 import javax.servlet.ServletException;
@@ -7,6 +6,7 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import java.io.*;
 import java.sql.Connection;
@@ -14,6 +14,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.*;
+
 
 /**
  * Servlet implementation class ProfileDispatcher
@@ -41,14 +42,35 @@ public class ProfileDispatcher extends HttpServlet {
         // TODO
     	response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
-    	String email = request.getParameter("email");
-		User profile = GetProfile.getProfile(email);  //Get Profile from Search
+    	String email = request.getParameter("email2");
+    	HttpSession session2 = request.getSession(false);
+		
+		String emailLogin = null;
+		
+		
+		
+		if(session2!=null) {
+			emailLogin = (String) session2.getAttribute("email");
+			if(request.getParameter("email2") == null) {
+				email = (String) request.getAttribute("email2");
+			}
+			if(request.getParameter("you") != null) {
+				email = emailLogin;
+			} 
+			request.setAttribute("areFriends", Verify.areFriends(emailLogin, email));
+			request.setAttribute("sentRequest", Verify.sentRequest(emailLogin,  email));
+		}
+		
+		User profile = GetProfile.getProfile(email); 
 		request.setAttribute("name", profile.getName());
 		request.setAttribute("email", profile.getEmail());
 		request.setAttribute("github", profile.getGithub());
 		request.setAttribute("company", profile.getCompany());
 		RequestDispatcher requestdispatcher = getServletContext().getRequestDispatcher("/Profile.jsp");
 		requestdispatcher.forward(request, response);
+	
+		
+		
 		
     }
 
