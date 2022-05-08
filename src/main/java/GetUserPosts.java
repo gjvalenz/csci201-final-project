2f3805ff12a16b2da5921b8e37b21bbf0576a6b7
David Morales <davidm62@usc.edu>
2022-05-08 01:12:00 -0700

    Update and rename src/main/java/finalproject/GetUserPosts.java to src/main/java/GetUserPosts.java

--- src/main/java/GetUserPosts.java
+++ src/main/java/GetUserPosts.java
@@ -1,4 +1,4 @@
-package finalproject;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
@@ -6,10 +6,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
-import Util.Constant;
-import Util.UserJobs;
-
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.Serial;
