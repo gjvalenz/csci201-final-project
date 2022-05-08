831221ae18ecd1df7096e563da977cb996f2529c
David Morales <davidm62@usc.edu>
2022-05-08 01:23:38 -0700

    Update and rename src/main/java/finalproject/Verify.java to src/main/java/Verify.java

--- src/main/java/Verify.java
+++ src/main/java/Verify.java
@@ -1,11 +1,8 @@
-package finalproject;
+//package Util; // commented out
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import Util.Constant;
-
 import java.io.IOException;
 import java.sql.SQLException;
 
@@ -121,7 +118,7 @@ public class Verify{
 		}
 
         
-        String SQL = "SELECT name FROM final.profile_info INNER JOIN final.user ON final.profile_info.profile_id=final.user.profile_id WHERE email='" + email +"';";
+        String SQL = "SELECT name FROM profile_info INNER JOIN user ON profile_info.profile_id=user.profile_id WHERE email='" + email +"';";
         
         
         /*SELECT * FROM PA4Users.Restaurant INNER JOIN PA4Users.Category ON PA4Users.Category.restaurant_id=PA4Users.Restaurant.restaurant_id INNER JOIN PA4Users.Restaurant_details ON PA4Users.Restaurant_details.details_id=restaurant.id INNER JOIN PA4Users.Rating_details ON PA4Users.Rating_details.rating_id=PA4Users.Restaurant.id"
@@ -173,7 +170,7 @@ public class Verify{
 		}
 
         
-        String SQL = "SELECT user_id FROM final.user WHERE email='" + email +"';";
+        String SQL = "SELECT user_id FROM user WHERE email='" + email +"';";
         
         Statement stmt = null;
         
@@ -188,6 +185,49 @@ public class Verify{
         
         if(rs.next()) {
         	s = rs.getInt("user_id"); 
+        }
+        
+        return s;
+    	
+    	
+    
+    }
+    
+    public static String getEmail(int id) throws SQLException {
+        //TODO
+    	
+    	try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+        Connection connection = null;
+        
+        try {
+			connection = DriverManager.getConnection(Constant.DBURL, Constant.DBUserName, Constant.DBPassword); //user,password
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+        
+        String SQL = "SELECT email FROM user WHERE user_id='" + id +"';";
+        
+        Statement stmt = null;
+        
+        stmt = connection.createStatement();
+        
+        ResultSet rs = null;
+        
+        rs = stmt.executeQuery(SQL);
+        
+        
+        String s = null;
+        
+        if(rs.next()) {
+        	s = rs.getString("email"); 
         }
         
         return s;
@@ -213,7 +253,7 @@ public class Verify{
     	
     	Connection connection = null;
     	
-    	String SQL = "SELECT passkey FROM final.user WHERE email='" + email +"';";
+    	String SQL = "SELECT passkey FROM user WHERE email='" + email +"';";
          
     	try {
 			connection = DriverManager.getConnection(Constant.DBURL, Constant.DBUserName, Constant.DBPassword); //"com.mysql.jdbc.Driver", url, username, password
@@ -261,58 +301,88 @@ public class Verify{
             throws ServletException, IOException {
         //TODO
 
+    	Connection connection = null;
+    	Statement stmt = null;
+    	ResultSet rs = null;
+    	String sql = null;
+    	String s = null;
     	try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
+			connection = DriverManager.getConnection(Constant.DBURL, Constant.DBUserName, Constant.DBPassword);
+			stmt = connection.createStatement();
+			sql = "SELECT * FROM user WHERE email='" + email +"'";
+    		rs = stmt.executeQuery(sql);
+    		if(rs.next()) {
+   			 	s = rs.getString("email");
+   			}
+    		if(s==null) {
+    			return false;
+    		}
+    		else if(s.equals(email)) {
+           	 return true;
+            }
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
-    	
-    	Connection connection = null;
-		try {
-			connection = DriverManager.getConnection(Constant.DBURL,  Constant.DBUserName, Constant.DBPassword); //"com.mysql.jdbc.Driver", url, username, password
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} //"jdbc:mysql://localhost/tutorial"
-    	Statement stmt = null;
-		try {
-			stmt = connection.createStatement();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-    	String SQL = "SELECT * FROM user WHERE email='" + email +"'";
-    	ResultSet rs = null;
-    	
-    	//boolean r = false;
-		try {
-			rs = stmt.executeQuery(SQL);
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		String s = null;
-		
-		try {
-			if(rs.next()) {
-			 s = rs.getString("email");
-			}
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-        
-        
-		if(s==null) {
-			return false;
-		}
-		else if(s.equals(email)) {
-       	 return true;
-        }
         
         
         return false;
     }
+    
+    public static boolean areFriends(String email1, String email2) {
+    	Connection connection = null;
+    	Statement stmt = null;
+    	ResultSet rs = null;
+    	String sql = null;
+    	try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			connection = DriverManager.getConnection(Constant.DBURL, Constant.DBUserName, Constant.DBPassword);
+			stmt = connection.createStatement();
+			sql = "SELECT * FROM friend WHERE friend1 = '" + getUserID(email1) + "' AND friend2 = '" + getUserID(email2) + "';";
+    		rs = stmt.executeQuery(sql);
+    		if(rs.next()) {
+    			return true;
+    		}
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+    	return false;	
+    	
+    }
+    
+    public static boolean sentRequest(String email1, String email2) {
+    	Connection connection = null;
+    	Statement stmt = null;
+    	ResultSet rs = null;
+    	String sql = null;
+    	try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			connection = DriverManager.getConnection(Constant.DBURL, Constant.DBUserName, Constant.DBPassword);
+			stmt = connection.createStatement();
+			sql = "SELECT * FROM friend_request WHERE uffrom = '" + getUserID(email1) + "' AND ufto = '" + getUserID(email2) + "';";
+    		rs = stmt.executeQuery(sql);
+    		if(rs.next()) {
+    			return true;
+    		}
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+    	return false;	
+    	
+    }
+  
 }
