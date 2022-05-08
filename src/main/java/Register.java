b15351e53cfc86fae3afdb69c8def617b583df16
David Morales <davidm62@usc.edu>
2022-05-08 01:19:28 -0700

    Update and rename src/main/java/finalproject/Register.java to src/main/java/Register.java

--- src/main/java/Register.java
+++ src/main/java/Register.java
@@ -1,5 +1,3 @@
-package finalproject;
-
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
@@ -16,8 +14,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import Util.Constant;
-import Util.UserJobs;
 
 /**
  * API
