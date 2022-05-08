5e9739255aab45754efe2c8c197ff2a758a05da9
David Morales <davidm62@usc.edu>
2022-05-08 01:14:55 -0700

    Update and rename src/main/java/finalproject/Login.java to src/main/java/Login.java

--- src/main/java/Login.java
+++ src/main/java/Login.java
@@ -1,4 +1,3 @@
-package finalproject;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -15,9 +14,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
-import Util.Constant;
-import Util.UserJobs;
 
 /**
  * API
